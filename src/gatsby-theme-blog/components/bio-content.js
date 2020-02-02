fc8824e39f8bfcbfef17fb93cf6fdd8e8921e263
Aisha Blake <aisha.g.blake@gmail.com>
2020-02-02 10:25:58 -0800

    Shadow bio-content.js

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+export default () => (
+  <Fragment>
+    Words by <Styled.a href="https://aisha.codes/">Aisha Blake</Styled.a>.
+    <br />
+    A theater kid turned tech community leader, she approaches speaking and teaching as a way to give others the tools to shine as brightly as they can.
+  </Fragment>
+)
